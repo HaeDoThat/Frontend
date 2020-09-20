5eca79a7035d6dc20be7c9a2b507fc7a5150cee2
Sonwanseo <sonwanseo@gmail.com>
2020-09-20 22:49:31 +0900

    [MODIFY] add Login URL & find login input bug

--- src/Components/Main/Project/Project.js
+++ src/Components/Main/Project/Project.js
@@ -4,7 +4,7 @@ import * as S from "./style";
 const img =
   "https://lh3.googleusercontent.com/proxy/xvGkSCQ0ZRrPA3rkNhfNwh8zIaPNWz3Vp9tBj_ib-gRStATmJQD5WhcBKyOGInZPJ4HHtVCVy_3IEhqzIbpsGvGDzi7T6bZpmkjMLyDKEnlBFObUyU5MoYNFVCFjCcRwHdWygkMmkHnxMcHYqNBvVQaOEF5WkabgKn3YPK9T5tcmpsdxV3JI5aEiaO7MUpkBRE0";
 
-function Project() {
+function Project({ id }) {
   return (
     <S.MainWrapper>
       <S.Image src={img} />
